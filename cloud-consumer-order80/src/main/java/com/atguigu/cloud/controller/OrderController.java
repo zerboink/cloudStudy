82d97be0c88b7f991ae91af5b69b2df3a3de7de8
zerboink <846434665@qq.com>
2024-07-21 15:05:16 +0800

    base+consul

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/cloud/controller/OrderController.java
@@ -0,0 +1,52 @@
+package com.atguigu.cloud.controller;
+
+import com.atguigu.cloud.entities.PayDTO;
+import com.atguigu.cloud.resp.ResultData;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @auther zzyy
+ * @create 2023-11-04 16:00
+ */
+@RestController
+public class OrderController{
+//    public static final String PaymentSrv_URL = "http://localhost:8001";//先写死，硬编码
+    public static final String PaymentSrv_URL = "http://cloud-payment-service";//服务注册中心上的微服务名称
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    /**
+     * 一般情况下，通过浏览器的地址栏输入url，发送的只能是get请求
+     * 我们底层调用的是post方法，模拟消费者发送get请求，客户端消费者
+     * 参数可以不添加@RequestBody
+     * @param payDTO
+     * @return
+     */
+    @GetMapping("/consumer/pay/add")
+    public ResultData addOrder(PayDTO payDTO){
+        return restTemplate.postForObject(PaymentSrv_URL + "/pay/add",payDTO,ResultData.class);
+    }
+    // 删除+修改操作作为家庭作业，O(∩_∩)O。。。。。。。
+
+    @DeleteMapping(value = "/consumer/pay/del/{id}")
+    public ResultData<String> deletePay(@PathVariable("id") Integer id) {
+        restTemplate.delete(PaymentSrv_URL + "/pay/del/"+id);
+        return ResultData.success("删除成功id为: " + id);
+    }
+
+    @PutMapping(value = "/consumer/pay/update")
+    public ResultData<String> updatePay(@RequestBody PayDTO payDTO){
+        restTemplate.put(PaymentSrv_URL + "/pay/update", payDTO, ResultData.class);
+        return ResultData.success("修改成功id为: ");
+    }
+
+    @GetMapping("/consumer/pay/get/{id}")
+    public ResultData getPayInfo(@PathVariable("id") Integer id){
+        return restTemplate.getForObject(PaymentSrv_URL + "/pay/get/"+id, ResultData.class, id);
+    }
+}
---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
@@ -0,0 +1,88 @@
+package com.atguigu.cloud.controller;
+
+import com.atguigu.cloud.entities.Pay;
+import com.atguigu.cloud.entities.PayDTO;
+import com.atguigu.cloud.resp.ResultData;
+import com.atguigu.cloud.resp.ReturnCodeEnum;
+import com.atguigu.cloud.service.PayService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+/**
+ * @auther zzyy
+ * @create 2023-11-03 18:55
+ */
+@RestController
+@Tag(name = "支付微服务模块",description = "支付CRUD")
+public class PayController{
+    @Resource PayService payService;
+
+    @PostMapping(value = "/pay/add")
+    @Operation(summary = "新增",description = "新增支付流水方法,json串做参数")
+    public ResultData<String> addPay(@RequestBody Pay pay){
+        System.out.println(pay.toString());
+        int i = payService.add(pay);
+        return ResultData.success("成功插入记录，返回值: " + i);
+    }
+
+    @DeleteMapping(value = "/pay/del/{id}")
+    @Operation(summary = "删除",description = "删除支付流水方法")
+    public ResultData<Integer> deletePay(@PathVariable("id") Integer id) {
+        int i = payService.delete(id);
+        return ResultData.success(i);
+    }
+
+    @PutMapping(value = "/pay/update")
+    @Operation(summary = "修改",description = "修改支付流水方法")
+    public ResultData<String> updatePay(@RequestBody PayDTO payDTO){
+        Pay pay = new Pay();
+        BeanUtils.copyProperties(payDTO, pay);
+
+        int i = payService.update(pay);
+        return ResultData.success("成功修改记录，返回值: " + i);
+    }
+
+    @GetMapping(value = "/pay/get/{id}")
+    @Operation(summary = "按照ID查流水",description = "查询支付流水方法")
+    public ResultData<Pay> getById(@PathVariable("id") Integer id){
+        if(id < 0) throw new RuntimeException("id不能为负数");
+
+        Pay pay = payService.getById(id);
+        return ResultData.success(pay);
+    }
+
+    @GetMapping(value = "/pay/getAll")
+    @Operation(summary = "查看全部流水",description = "查询全部支付流水方法")
+    public ResultData<List<Pay>> getAll(){
+        List<Pay> res = payService.getAll();
+        return ResultData.success(res);
+    }
+
+    @RequestMapping(value = "/pay/error",method = RequestMethod.GET)
+    public ResultData<Integer> getPayError()
+    {
+        Integer i = Integer.valueOf(200);
+        try
+        {
+            System.out.println("--------come here");
+            int data = 10/0;
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResultData.fail(ReturnCodeEnum.RC500.getCode(),e.getMessage());
+        }
+        return ResultData.success(i);
+    }
+
+    @Value("${server.port}")
+    private String port;
+    @GetMapping(value = "/pay/get/info")
+    public String getInfoByConsul(@Value("${atguigu.info}") String atguiguInfo)
+    {
+        return "atguiguInfo: "+atguiguInfo+"\t"+"port: "+port;
+    }
+}
